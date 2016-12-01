b67930f300c35161348bfb3423f29becb2e234be
mirv <reski.raptor@gmail.com>
2016-12-01 19:39:13 +0000

    root route, readme, 3 layers of model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :countries
   resources :regions
   resources :continents
+  root 'contients/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
