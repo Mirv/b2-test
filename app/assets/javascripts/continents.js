3b3d0663eb208e83c380079891c76bd5014d627f
mirv <reski.raptor@gmail.com>
2016-12-07 14:12:58 +0000

    Fixed the dynamic jquery which needed .nested

--- app/assets/javascripts/continents.js
+++ app/assets/javascripts/continents.js
@@ -1,32 +1,80 @@
 $(document).ready(function() {
-
-    $("#region a.add_fields").
+    $("#owner a.add_fields").
       data("association-insertion-position", 'before').
       data("association-insertion-node", 'this');
 
-    $('#region').bind('cocoon:before-insert', function(e,task_to_be_added) {
+    $('#owner').bind('cocoon:after-insert',
+         function() {
+           $("#owner_from_list").hide();
+           $("#owner a.add_fields").hide();
+         });
+    $('#owner').bind("cocoon:after-remove",
+         function() {
+           $("#owner_from_list").show();
+           $("#owner a.add_fields").show();
+         });
+
+    $("#tags a.add_fields").
+      data("association-insertion-position", 'before').
+      data("association-insertion-node", 'this');
+
+    $('#tags').bind('cocoon:after-insert',
+         function(e, tag) {
+             console.log('inserting new tag ...');
+             $(".project-tag-fields a.add-tag").
+                 data("association-insertion-position", 'after').
+                 data("association-insertion-node", 'this');
+             $(this).find('.project-tag-fields').bind('cocoon:after-insert',
+                  function() {
+                    console.log('insert new tag ...');
+                    console.log($(this));
+                    $(this).find(".tag_from_list").remove();
+                    $(this).find("a.add_fields").hide();
+                  });
+         });
+
+    $('.project-tag-fields').bind('cocoon:after-insert',
+        function(e) {
+            console.log('replace OLD tag ...');
+            e.stopPropagation();
+            console.log($(this));
+            $(this).find(".tag_from_list").remove();
+            $(this).find("a.add_fields").hide();
+        });
+
+    $('#tasks').bind('cocoon:before-insert', function(e,task_to_be_added) {
         console.log(task_to_be_added);
         task_to_be_added.fadeIn('slow');
     });
 
-    $('#region').bind('cocoon:before-remove', function(e, task) {
+    $('#tasks').bind('cocoon:after-insert', function(e, added_task) {
+        //added_task.css("background","red");
+    });
+
+    $('#tasks').bind('cocoon:before-remove', function(e, task) {
         $(this).data('remove-timeout', 1000);
         task.fadeOut('slow');
     })
     
-        $("#country a.add_fields").
-      data("association-insertion-position", 'before').
-      data("association-insertion-node", 'this');
-
-    $('#country').bind('cocoon:before-insert', function(e,task_to_be_added) {
-        console.log(task_to_be_added);
-        task_to_be_added.fadeIn('slow');
+    // -- Mine -- //
+    // -- Mine -- //
+    // -- Mine -- //
+    
+    $('#regions').bind('cocoon:before-insert', function(e,region_to_be_added) {
+        console.log(region_to_be_added);
+        region_to_be_added.fadeIn('slow');
     });
 
-    $('#country').bind('cocoon:before-remove', function(e, task) {
+    $('#regions').bind('cocoon:after-insert', function(e, added_region) {
+        added_region.css("background","red");
+    });
+
+    $('#regions').bind('cocoon:before-remove', function(e, region) {
+        region.css("background","red");
         $(this).data('remove-timeout', 1000);
-        task.fadeOut('slow');
+        region.fadeOut('slow');
     })
+
 
     //$('body').tabs();
 });
